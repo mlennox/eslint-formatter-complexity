842b6c82c3675e93aa360cfaf1a83339bb92df78
Mark Lennox <mark@webpusher.ie>
2019-05-05 16:00:57 +0100

    two basic tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,39 @@ import index from './index';
 
 describe('complexity index', () => {
   test('if no matching rules in results return appropriate message', () => {
-    const meta = { rulesMeta: {} };
+    const meta = {
+      rulesMeta: {
+        'not-a-complexity-metric-rule': {},
+        'also-not-complexity-metric': {},
+      },
+    };
     const expected = 'None of the rules we want were used';
     const output = index(null, meta);
     expect(output).toEqual(expected);
   });
-  test('if no messages from complexity rules then return appropriate message', () => {});
+  test('if no messages from complexity rules then return appropriate message', () => {
+    // list of all the rules that were run against the codebase
+    const meta = {
+      rulesMeta: {
+        'not-a-complexity-metric-rule': {},
+        'also-not-complexity-metric': {},
+        complexity: {},
+      },
+    };
+    // list of rule violations in the codebase
+    const results = [
+      {
+        filePath: 'path/to/source.one.js',
+        messages: [
+          {
+            ruleId: 'not-a-complexity-metric-rule',
+          },
+        ],
+      },
+    ];
+    const expected = 'No complexity issues were found';
+    const output = index(results, meta);
+    expect(output).toEqual(expected);
+  });
   // test('');
 });
