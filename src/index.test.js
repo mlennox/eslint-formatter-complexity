8b086c0ccfb18ebae5792e19574aa9ce9c9f7088
Mark Lennox <mark@webpusher.ie>
2019-05-05 15:33:33 +0100

    fixing jest import, bumping package version

--- src/index.test.js
+++ src/index.test.js
@@ -4,9 +4,9 @@ describe('complexity index', () => {
   test('if no matching rules in results return appropriate message', () => {
     const meta = { rulesMeta: {} };
     const expected = 'None of the rules we want were used';
-    const output = index(null, rulesMeta);
+    const output = index(null, meta);
     expect(output).toEqual(expected);
   });
-  test('if no messages fromm complexity rules then return appropriate message', () => {});
-  test('');
+  test('if no messages from complexity rules then return appropriate message', () => {});
+  // test('');
 });
