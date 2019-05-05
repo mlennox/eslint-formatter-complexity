270a06fc658c439c8a3a91b19822cc4a15f73be0
Mark Lennox <mark@webpusher.ie>
2019-05-05 14:37:41 +0100

    stubbing tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,12 @@
+import index from './index';
+
 describe('complexity index', () => {
-  test('', () => {});
+  test('if no matching rules in results return appropriate message', () => {
+    const meta = { rulesMeta: {} };
+    const expected = 'None of the rules we want were used';
+    const output = index(null, rulesMeta);
+    expect(output).toEqual(expected);
+  });
+  test('if no messages fromm complexity rules then return appropriate message', () => {});
+  test('');
 });
